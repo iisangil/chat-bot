2f56e369c748011af05d712f9d9de012d84391bf
Lee <iisangil@umich.edu>
2023-03-07 16:44:44 -0500

    initial commit with watcher bot

---
+++ chat.py
@@ -0,0 +1,8 @@
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
---
+++ watcher.py
@@ -0,0 +1,34 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('WATCHER_TOKEN')
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message_edit(before, after):
+    if before.author == client.user:
+        return
+    
+    await before.channel.send(f'I see all in the universe. {before.author.display_name} originally said `{before.content}`, not `{after.content}`!')
+
+@client.event
+async def on_message_delete(message):
+    if message.author == client.user:
+        return
+    
+    await message.channel.send(f'I see all in the universe. {message.author.display_name} said `{message.content}`, before deleting!')
+
+client.run(TOKEN)
